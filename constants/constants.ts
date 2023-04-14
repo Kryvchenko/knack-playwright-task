d4053a50a692f30c796e7742ec8de79db7e22447
Kryvchenko <amzkryvchenko@gmail.com>
2023-04-15 00:15:20 +0200

    workflow updated

--- constants/constants.ts
+++ constants/constants.ts
@@ -1,6 +1,6 @@
 export const LOGIN_DETAILS = Object.freeze({
-  email: 'amzkryvchenko@gmail.com',
-  password: 'P@$Fs7whDS#f55V',
+  email: 'example@gmail.com',
+  password: 'your password',
   liveAppUsername: 'admin@test.com',
   liveAppPwd: 'test',
 });
