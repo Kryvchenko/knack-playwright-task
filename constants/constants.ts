7ad2ad7d2e301a06c4b6b1d82c79c3dd2de96993
Oleksandr <94463783+Kryvchenko@users.noreply.github.com>
2023-04-15 10:15:56 +0200

    Update constants.ts

--- constants/constants.ts
+++ constants/constants.ts
@@ -1,6 +1,6 @@
 export const LOGIN_DETAILS = Object.freeze({
-  email: 'amzkryvchenko@gmail.com',
-  password: 'P@$Fs7whDS#f55V',
+  email: 'example@gmail.com',
+  password: 'example',
   liveAppUsername: 'admin@test.com',
   liveAppPwd: 'test',
 });
